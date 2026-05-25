52a169c54c99c6187be7b190fb61bc5b68f245a6
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-05-25 10:41:40 +0530

    Add AWS region and instance ID variables

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "ap-south-1"   # Mumbai region
+}
+
+variable "instance_id" {
+  description = "EC2 instance to monitor"
+  default     = "i-xxxxxxxxxxxxxxxxx"
+}
