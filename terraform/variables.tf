c1ff9688b094b7ec968bffd047af5b4731ec7943
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-05-26 09:25:08 +0530

    Enhance variables.tf with additional variables
    Added new variables for project configuration and monitoring thresholds.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,58 @@
 variable "aws_region" {
-  default = "ap-south-1"   # Mumbai region
+  description = "AWS region to deploy all resources"
+  type        = string
+  default     = "ap-south-1"
 }
 
-variable "instance_id" {
-  description = "EC2 instance to monitor"
-  default     = "i-xxxxxxxxxxxxxxxxx"
+variable "project_name" {
+  description = "Prefix used to name all AWS resources"
+  type        = string
+  default     = "aws-health-monitor"
+}
+
+variable "ec2_instance_id" {
+  description = "ID of the EC2 instance to monitor (e.g. i-0a1b2c3d4e5f67890)"
+  type        = string
+}
+
+variable "rds_instance_id" {
+  description = "ID of the RDS instance to monitor"
+  type        = string
+  default     = ""
+}
+
+variable "cpu_alarm_threshold" {
+  description = "CPU utilization % that triggers the EC2 alarm"
+  type        = number
+  default     = 80
+}
+
+variable "rds_connection_threshold" {
+  description = "Number of DB connections that triggers the RDS alarm"
+  type        = number
+  default     = 100
+}
+
+variable "alarm_evaluation_periods" {
+  description = "Number of consecutive periods before alarm fires"
+  type        = number
+  default     = 2
+}
+
+variable "alarm_period_seconds" {
+  description = "How often CloudWatch checks the metric (in seconds)"
+  type        = number
+  default     = 60
+}
+
+variable "alert_email" {
+  description = "Email address to receive SNS notifications"
+  type        = string
+}
+
+variable "slack_webhook_url" {
+  description = "Slack incoming webhook URL for notifications"
+  type        = string
+  sensitive   = true
+  default     = ""
 }
