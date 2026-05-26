a311e7f17b921921fde4593ea88a35dee1c92037
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-05-26 12:17:28 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,10 +3,10 @@ output "ec2_lambda_arn" {
   value       = aws_lambda_function.ec2_remediation.arn
 }
 
-output "rds_lambda_arn" {
+/*output "rds_lambda_arn" {
   description = "ARN of the RDS remediation Lambda function"
   value       = aws_lambda_function.rds_remediation.arn
-}
+}*/
 
 output "sns_topic_arn" {
   description = "ARN of the SNS notification topic"
@@ -23,10 +23,10 @@ output "ec2_status_alarm_name" {
   value       = aws_cloudwatch_metric_alarm.ec2_status_alarm.alarm_name
 }
 
-output "rds_alarm_name" {
+/*output "rds_alarm_name" {
   description = "Name of the RDS connections CloudWatch alarm"
   value       = aws_cloudwatch_metric_alarm.rds_connection_alarm.alarm_name
-}
+}*/
 
 output "lambda_iam_role_arn" {
   description = "IAM role ARN used by Lambda functions"
