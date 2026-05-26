4d42f51416ad2f96de7f75ed9befcb161c7e3abd
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-05-26 09:11:58 +0530

    Update ec2_remediation.py

--- lambda/ec2_remediation.py
+++ lambda/ec2_remediation.py
@@ -20,3 +20,17 @@ def lambda_handler(event, context):
         "status": "remediated",
         "instance": instance_id
     }
+    
+# Update ec2_remediation.py to call notification
+from notification import send_slack_alert
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2')
+    instance_id = event['detail']['instance-id']
+    
+    ec2.reboot_instances(InstanceIds=[instance_id])
+    
+    # Send Slack alert
+    send_slack_alert(instance_id, "EC2 Reboot")      # ← Add this
+    
+    return {"status": "remediated", "instance": instance_id}
