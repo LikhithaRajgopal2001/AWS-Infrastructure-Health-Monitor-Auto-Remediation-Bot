aec7ac99494b229959520dd8bb8e3a02cca0d6d6
Likhitha <71483520+LikhithaRajgopal2001@users.noreply.github.com>
2026-05-26 09:24:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,27 +2,46 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Lambda function
-resource "aws_lambda_function" "remediation_bot" {
-  filename      = "lambda/ec2_remediation.zip"
-  function_name = "aws-health-monitor"
+resource "aws_lambda_function" "ec2_remediation" {
+  filename      = "../lambda/ec2_remediation.zip"
+  function_name = "${var.project_name}-ec2-remediation"
   role          = aws_iam_role.lambda_role.arn
   handler       = "ec2_remediation.lambda_handler"
   runtime       = "python3.11"
-}
+  timeout       = 30
 
-# CloudWatch Alarm
-resource "aws_cloudwatch_metric_alarm" "cpu_alarm" {
-  alarm_name          = "high-cpu-alarm"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = 60
-  evaluation_periods  = 2
-  threshold           = 80
-  comparison_operator = "GreaterThanThreshold"
-  alarm_actions       = [aws_sns_topic.alerts.arn]
-
-  dimensions = {
-    InstanceId = var.instance_id
+  environment {
+    variables = {
+      SLACK_WEBHOOK_URL = var.slack_webhook_url
+      SNS_TOPIC_ARN     = aws_sns_topic.alerts.arn
+    }
   }
+
+  depends_on = [aws_iam_role_policy.lambda_policy]
 }
+
+resource "aws_lambda_function" "rds_remediation" {
+  filename      = "../lambda/rds_remediation.zip"
+  function_name = "${var.project_name}-rds-remediation"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "rds_remediation.lambda_handler"
+  runtime       = "python3.11"
+  timeout       = 30
+
+  environment {
+    variables = {
+      SLACK_WEBHOOK_URL = var.slack_webhook_url
+      SNS_TOPIC_ARN     = aws_sns_topic.alerts.arn
+    }
+  }
+
+  depends_on = [aws_iam_role_policy.lambda_policy]
+}
+
+resource "aws_cloudwatch_event_rule" "ec2_alarm_rule" {
+  name        = "${var.project_name}-ec2-alarm-rule"
+  description = "Trigger EC2 remediation Lambda when CloudWatch alarm fires"
+
+  event_pattern = jsonencode({
+    source      = ["aws.cloudwatch"]
+    detail-type
